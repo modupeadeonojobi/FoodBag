86dbf625ca0444323b74a5719fbad951fdc3023b
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2021-08-05 11:44:14 +0100

    Add responsiveness

--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -10,20 +10,18 @@ class CartList extends Component {
         const { counters } = this.props;
 
         return (
-            <div className="">
-                <div className="delivery">
-                    <img className="img-fluid img-thumbnail pr-3"
-                        src={close} alt="Icon" onClick={() => this.props.onDelete(counters.id)}
-                    />
-                    <img className="img-fluid"
-                        src={this.props.imageUrl} alt="Icon"
-                    />
-                    <div className="" >
-                        <h6 className="ml-5 c_font">{this.props.name}</h6>
-                        <img className="ml-5" src={plus} alt="icon" onClick={() => this.props.onIncrement(counters)} /> {counters.value}
-                        <img className="pr-5 mr-5 pl-2" src={minus} alt="icon" onClick={() => this.props.onDecrement(counters)} />
-                        <span className="price">N{this.props.price}</span>
-                    </div>
+            <div className="delivery">
+                <img className="img-fluid img-thumbnail pr-3"
+                    src={close} alt="Icon" onClick={() => this.props.onDelete(counters.id)}
+                />
+                <img className="img-fluid"
+                    src={this.props.imageUrl} alt="Icon"
+                />
+                <div className="pb-4 items_info" >
+                    <h6 className="ml-5 c_font">{this.props.name}</h6>
+                    <img className="ml-5" src={plus} alt="icon" onClick={() => this.props.onIncrement(counters)} /> <span className="qty">{counters.value}</span>
+                    <img className="pr-5 mr-5 pl-2" src={minus} alt="icon" onClick={() => this.props.onDecrement(counters)} />
+                    <span className="price">N{this.props.price}</span>
                 </div>
             </div>
         );
--- src/components/Location/Select.js
+++ src/components/Location/Select.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
-
+import './Select.css';
 class Select extends Component {
     state = {}
     render() {
         return (
             <div className="form-group">
                 <label>Where are you?</label>
-                <select className="form-select form-select-lg mb-3 form-control" aria-label=".form-select-lg example">
+                <select className="form-select form-select-lg mb-3 form-control" >
                     <option selected>Bodija Market</option>
                     <option value="1">Olabale Market</option>
                 </select>
-            </div>
+            </div >
         );
     }
 }
